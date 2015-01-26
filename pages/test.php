98fc112a04333f11138665dc90698f88781d554b
thobiasmalmqvist <malmqvistt@gmail.com>
2015-01-26 11:06:08 +0100

    Create test.php

---
+++ pages/test.php
@@ -0,0 +1,4 @@
+<h1>Programmeringstest Thobias Malmqvist, januari 2015</h1>
+<p>Välkommen!</p>
+<p>Använd länkarna ovan för att ladda Wikipedia-artiklar. Välj en artikel i
+listan ovan för att visa innehållet.</p>
