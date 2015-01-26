87c9828b651d2ffd9f8240d454698ad3e37f7bbf
thobiasmalmqvist <malmqvistt@gmail.com>
2015-01-26 11:02:46 +0100

    Create printList.js

---
+++ js/printList.js
@@ -0,0 +1,7 @@
+function addToEmpty() {
+	for (var i=0; i<titleList.length; i++){
+		opt = document.createElement('option');
+		printList.push(opt.innerHTML = titleList[i]);
+		dropdown.appendChild(opt);
+	}
+}
