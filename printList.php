263f623b7a0a0de9f226736d943e678238d863fa
thobiasmalmqvist <malmqvistt@gmail.com>
2015-01-26 11:05:15 +0100

    Delete printList.php

--- printList.php
+++
@@ -1,9 +0,0 @@
-<script type="text/JavaScript">
-
-for (var i=0; i<titleList.length; i++){
-	opt = document.createElement('option');
-	printList.push(opt.innerHTML = titleList[i]);
-	dropdown.appendChild(opt);
-}
-
-</script>
