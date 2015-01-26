2a28ea86034f170ff0aa651d94f945583cb7f0be
thobiasmalmqvist <malmqvistt@gmail.com>
2015-01-26 11:05:44 +0100

    Create addCategory.php

---
+++ addCategory.php
@@ -0,0 +1,11 @@
+<p>
+
+<script type="text/JavaScript">
+$('#add_categoryID').submit(function (){
+	var newCategory = $('#category').val();
+	$('#category_feedback').html('Ny kategori tillagd: ' + newCategory);
+});
+
+</script>
+
+</p>
