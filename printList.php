2e08f3228e06ed76f7ded3a705fe98dc9a7bfe3b
thobiasmalmqvist <malmqvistt@gmail.com>
2015-01-26 11:04:42 +0100

    Create printList.php

---
+++ printList.php
@@ -0,0 +1,9 @@
+<script type="text/JavaScript">
+
+for (var i=0; i<titleList.length; i++){
+	opt = document.createElement('option');
+	printList.push(opt.innerHTML = titleList[i]);
+	dropdown.appendChild(opt);
+}
+
+</script>
